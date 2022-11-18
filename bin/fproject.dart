f6c84cbf4862c9904b2a5cd00aff0e8fc3877792
Achal-mk <achalmottah22@gmail.com>
2022-11-18 15:52:47 +0530

    commit

---
+++ bin/fproject.dart
@@ -0,0 +1,5 @@
+import 'package:fproject/fproject.dart' as fproject;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fproject.calculate()}!');
+}
---
+++ test/fproject_test.dart
@@ -0,0 +1,8 @@
+import 'package:fproject/fproject.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
